55631e6199dfb47e55dc78caf4cee106635092ab
blazerbots <ohsblazerbots@gmail.com>
2025-01-11 15:55:54 -0700

    Work in progress for subsystem.

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -1,5 +1,45 @@
 package frc.robot.subsystems;
 
-public class CoralSubsystem {
-    
+import frc.robot.Constants.ElevatorConstants;
+
+
+import frc.robot.subsystems.motorController;
+import frc
+
+public class CoralSubsystem extends SubsystemBase 
+{
+
+
+
+
+
+
+//This is the hand
+    public handy()
+    {
+
+    }
+
+
+
+
+
+
+//This is the arm joint.
+    public armJoints()
+    {
+
+
+    }
+
+
+//This is the elevator
+    public lift()
+    {
+
+    }
+
+
 }
+    
+
