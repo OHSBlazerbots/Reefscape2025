604d715507319dc9b83be233ec5048fff52cc6f9
blazerbots <ohsblazerbots@gmail.com>
2025-03-04 17:28:11 -0700

    Removed an Extra Camera and made a new object for driver camera subsystem

--- src/main/java/frc/robot/subsystems/DriverCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriverCameraSubsystem.java
@@ -7,21 +7,17 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriverCameraSubsystem extends SubsystemBase {
-    UsbCamera frontBumper;
-    UsbCamera ShooterCam;
+    UsbCamera armCamera;
     NetworkTableEntry cameraSelection;
     int currentCameraIndex = 0;
     VideoSink cameraServer;
     UsbCamera[] allCameras;
 
     public DriverCameraSubsystem() {
-        frontBumper = CameraServer.startAutomaticCapture(0); // 0 is placeholder
-        ShooterCam = CameraServer.startAutomaticCapture(1); // 1 is placeholder
+        armCamera = CameraServer.startAutomaticCapture(0); // 0 is placeholder
         cameraServer = CameraServer.getServer();
-        allCameras = new UsbCamera[] { frontBumper, ShooterCam };
-        frontBumper.setResolution(424, 240);
-        frontBumper.setFPS(30);
-        ShooterCam.setResolution(160, 120);
-        ShooterCam.setFPS(20);
+        allCameras = new UsbCamera[] { armCamera, };
+        armCamera.setResolution(424, 240);
+        armCamera.setFPS(30);
     }
 }
