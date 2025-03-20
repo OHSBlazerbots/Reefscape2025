bb218d9f0e46a4a5220ab35fb61ce7b40ffe91ad
blazerbots <ohsblazerbots@gmail.com>
2025-03-20 10:45:39 -0600

    Uncommented setpink during periodic

--- src/main/java/frc/robot/subsystems/LightingSubsystem.java
+++ src/main/java/frc/robot/subsystems/LightingSubsystem.java
@@ -35,7 +35,7 @@ public class LightingSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // setPink();
+        setPink();
     }
 
     public void pinkWhiteStrobe() {
